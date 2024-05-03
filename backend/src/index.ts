96a0ae2b036f19f772e774525a51b418131b7ca4
SaurabhBudhwani26 <budhwanisaurabh@gmail.com>
2024-05-03 22:48:47 +0530

    Added routes for signup, signin and added authorization middleware in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,76 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { decode, sign, verify } from 'hono/jwt';
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>()
 
 
+app.use('/api/v1/blog/*', async (c, next) => {
+
+  const header = c.req.header("authorization") || "";
+  const token = header.split(" ")[1]
+  const response = await verify(token, c.env.JWT_SECRET);
+  if (response.id) {
+
+    await next()
+  }else{
+    c.status(403);
+    return c.json({error: "unauthorized"})
+  }
+})
 
 
-app.post('/api/v1/user/signup', (c) => {
+
+app.post('/api/v1/user/signup', async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
-  return c.text('Hello')
+  const body = await c.req.json()
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+
+    }
+  })
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+
+  return c.json({ jwt: token })
 
 })
 
-app.post('/api/v1/user/signin', (c) => {
+app.post('/api/v1/user/signin', async (c) => {
 
-  return c.text('Hello')
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+
+  const body = await c.req.json()
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+      password: body.password
+    }
+  })
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "User not found" })
+  }
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+
+  return c.json({ jwt: jwt })
 
 })
 
