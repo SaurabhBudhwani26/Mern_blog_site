2977922f8a8340e254efaab28f7718d20f7a66a1
SaurabhBudhwani26 <budhwanisaurabh@gmail.com>
2024-05-24 01:40:16 +0530

    Required changes to routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -15,9 +15,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/" element={<Signin />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/" element={<Blogs />} />
+        <Route path="/blogs" element={<Blogs />} />
         <Route path="/publish" element={<Publish />} />
       </Routes>
     </BrowserRouter>
